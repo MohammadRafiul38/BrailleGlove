c4c02fbb07067d3faf6584fa08c9bfc770fb3b6f
Mohammad Rafiul Chowdhury <mrafiulchowdhury@gmail.com>
2026-03-05 00:27:13 +0600

    Update brailleglove.ino

--- arduino/brailleglove.ino
+++ arduino/brailleglove.ino
@@ -1,7 +1,6 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BLE(2, 3); 
-
 int motorPins[6] = {4, 5, 6, 7, 8, 9};
 
 const byte brailleMap[26][6] = {
@@ -15,39 +14,25 @@ const byte brailleMap[26][6] = {
 };
 
 void setup() {
-  Serial.begin(9600); 
-  BLE.begin(9600);    
-  
-  for(int i = 0; i < 6; i++){
-    pinMode(motorPins[i], OUTPUT);
-    digitalWrite(motorPins[i], LOW);
-  }
-  Serial.println("Glove Ready. Use Serial Monitor or App to test.");
+  Serial.begin(9600);
+  BLE.begin(9600); 
+  for(int i = 0; i < 6; i++) pinMode(motorPins[i], OUTPUT);
+  Serial.println("System Live. Send 'a' from phone.");
 }
 
 void loop() {
-  char c = 0;
-  if (BLE.available()) { 
-    c = BLE.read(); 
-  } else if (Serial.available()) { 
-    c = Serial.read(); 
-  }
-
-  if (c != 0) {
-    c = tolower(c);
+  if (BLE.available()) {
+    char c = tolower(BLE.read());
     if (c >= 'a' && c <= 'z') {
       int idx = c - 'a';
       Serial.print("Vibrating for: ");
       Serial.println(c);
 
-      for (int p = 0; p < 3; p++) {
-        for (int i = 0; i < 6; i++) {
-          if (brailleMap[idx][i] == 1) digitalWrite(motorPins[i], HIGH);
-        }
-        delay(200);
-        for (int i = 0; i < 6; i++) digitalWrite(motorPins[i], LOW);
-        delay(150);
+      for (int i = 0; i < 6; i++) {
+        if (brailleMap[idx][i] == 1) digitalWrite(motorPins[i], HIGH);
       }
+      delay(500);
+      for (int i = 0; i < 6; i++) digitalWrite(motorPins[i], LOW);
     }
   }
 }
